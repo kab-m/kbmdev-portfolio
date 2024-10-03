190c8d71888b7f4a81a31554ba519d38ce1dd164
kbmDEV <tommi.rainer@gmail.com>
2024-10-03 17:57:10 +0100

    Project Setup

---
+++ src/components/Test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div className="w-full h-svh bg-red-100 flex justify-center items-center">
+      Test
+    </div>
+  );
+};
+
+export default Test;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Test from "../components/Test";
+
+function Homepage() {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+}
+
+export default Homepage;
